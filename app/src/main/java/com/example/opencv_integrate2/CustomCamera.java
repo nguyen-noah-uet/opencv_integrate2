f9e9567ad23ba3565a96c176098e052a28365c9f
nguyen-noah-uet <20020666@vnu.edu.vn>
2023-11-25 14:27:08 +0700

    Add sharpness

--- app/src/main/java/com/example/opencv_integrate2/CustomCamera.java
+++ app/src/main/java/com/example/opencv_integrate2/CustomCamera.java
@@ -5,10 +5,13 @@ import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CaptureRequest;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.ViewGroup;
 
 import org.opencv.android.JavaCamera2View;
 
 public class CustomCamera extends JavaCamera2View {
+    private static final String TAG = "CustomCamera";
     private final CameraManager manager;
 
     public CustomCamera(Context context, int cameraId) {
@@ -19,21 +22,31 @@ public class CustomCamera extends JavaCamera2View {
         super(context, attrs);
         manager = (CameraManager) getContext().getSystemService(Context.CAMERA_SERVICE);
     }
-    public void setAutoFocusMode(boolean autoFocus) {
+    public float getFocusDistance(){
         try {
-            if(autoFocus){
-                mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE,
-                        CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
+            return mPreviewRequestBuilder.get(CaptureRequest.LENS_FOCUS_DISTANCE);
+        }
+        catch (Exception e){
+            throw new RuntimeException(e);
+        }
+
+    }
+    public void setAutoFocus(boolean autoFocus) {
+        try {
+            if (mPreviewRequestBuilder == null) {
+                Log.i(TAG, "mPreviewRequestBuilder is null");
+                return;
             }
-            else {
-                mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE,
-                        CaptureRequest.CONTROL_AF_MODE_OFF);
-            }
+            if(autoFocus)
+                mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_OFF);
+            else
+                mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_OFF);
             mCaptureSession.setRepeatingRequest(mPreviewRequestBuilder.build(), null, mBackgroundHandler);
         } catch (CameraAccessException e) {
             throw new RuntimeException(e);
         }
     }
+
     public void setFocusDistance(float distance) {
         try {
             mPreviewRequestBuilder.set(CaptureRequest.LENS_FOCUS_DISTANCE, distance);
@@ -41,5 +54,27 @@ public class CustomCamera extends JavaCamera2View {
         } catch (CameraAccessException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    protected boolean connectCamera(int width, int height) {
+        boolean b = super.connectCamera(width, height);
+        if (mCaptureSession == null) {
+            Log.i(TAG, "mCaptureSession is null");
+            return b;
+        }
+        try {
+            mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_OFF);
+            mCaptureSession.setRepeatingRequest(mPreviewRequestBuilder.build(), null, mBackgroundHandler);
+        } catch (CameraAccessException e) {
+            Log.e(TAG, e.getMessage());
+        }
+        return b;
+    }
+
+    public enum FocusState {
+        FOCUSING,
+        FOCUSED,
+        NOT_FOCUSED
     }
 }
