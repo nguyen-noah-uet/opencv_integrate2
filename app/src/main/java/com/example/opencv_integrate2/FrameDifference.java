a40cb904477e91ae98b4a16bb6ee362c3120e494
Hoan-Quang <hoan08122002@gmail.com>
2023-12-04 17:54:03 +0700

    Fixed 1 vài thông số

--- app/src/main/java/com/example/opencv_integrate2/FrameDifference.java
+++ app/src/main/java/com/example/opencv_integrate2/FrameDifference.java
@@ -4,7 +4,6 @@ import android.util.Log;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
-import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
 public class FrameDifference {
